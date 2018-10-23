7e34e8281669da2fd938859c828ae2d7dedc324e
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-23 22:00:51 +0545

    Adding index file changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import Props from './Props';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
