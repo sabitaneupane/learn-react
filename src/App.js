8fadd56d36d5d4f00235ad909c60c2d577551c95
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-23 14:10:56 +0545

    Testing bootstrap setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ class App extends Component {
   render() {
     return (
       <div class="container"> 
-      	
+      	<div class="row">
+	      <div class="col-md-8"> 
+	      	...
+	      </div>
+	      
+	      <div class="col-md-4">
+	      	...
+	      </div>
+      	</div>
       </div>
     );
   }
