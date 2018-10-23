1b317b3d62e00982631ef03bd76b3fb692ca5f65
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-23 22:18:05 +0545

    Adding comment for Props work flow

--- src/Props.js
+++ src/Props.js
@@ -7,9 +7,15 @@ class Props extends Component {
     return (
       <div className="container"> 
         <p>
-          <b> Movie name: </b> {this.props.title}
+          <b> Movie name: </b> 
+             
+             {/* Adding movie title using Props */}
+             {this.props.title}
             <br/> 
-          <b> Movie Genre: </b> {this.props.genre}
+          <b> Movie Genre: </b> 
+
+             {/* Adding movie genre using Props */}
+            {this.props.genre}
         </p>
         <hr/>
       </div>
@@ -19,6 +25,9 @@ class Props extends Component {
 
 ReactDOM.render(
   <div>
+
+     {/* Passing movie title and genre using attribute */}
+     
     <Props title="The social networking" genre="Documentary"/>
     <Props title="Avatar" genre="Sci-fiction"/>
     <Props title="Titanic" genre="Drama, Romantic"/>
