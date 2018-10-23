9f1861904d168730217b4452ddfb0f803ec67f94
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-23 13:25:17 +0545

    project build: npm run-script build

---
+++ build/precache-manifest.4c067401929b783ef9284a41b1c9117f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e387f6b6dec7ebefe4b",
+    "url": "/static/js/main.8e387f6b.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "8e387f6b6dec7ebefe4b",
+    "url": "/static/css/main.593fb830.chunk.css"
+  },
+  {
+    "revision": "d085940fb95b8347ec6f3975755d62d5",
+    "url": "/index.html"
+  }
+];
