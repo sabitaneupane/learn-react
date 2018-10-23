1a19fbe887c58245d78e26557d49e0e48c3a57b0
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-23 14:07:34 +0545

    Boostrap link setup for project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div> Hello world!! </div>
+      <div class="container"> 
+      	
+      </div>
     );
   }
 }
