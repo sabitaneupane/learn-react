b65e22824ab234665fcbff27d841ed243e6c654c
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-24 15:54:46 +0545

    Adding comments to the state and setState

--- src/State.js
+++ src/State.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 
 
 class State extends Component{
+
 	constructor(){
+		{/* Initializing constructor and value of count*/}	
 		super();
 		this.state = { count:0 };
 	}
 
 	UpdateClickCount(){
+		{/* Incrementing number of button clikced*/}
 		this.setState( (prevState) => {
 			return { count: prevState.count + 1 };
 		});
@@ -17,7 +20,10 @@ class State extends Component{
 	render(){
 		return (
 			<div>
+				{/* Event handeler function to count number of button cliked */}	
 				<button onClick={() => this.UpdateClickCount()}> Click me </button>
+				
+				{/* State of count: number of button clicked*/}	
 				<p> Clicked {this.state.count} </p>
 			</div>
 
