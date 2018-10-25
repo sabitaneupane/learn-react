392bf0608d0d550f8f741a4478f9db4331117641
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-10-25 10:41:22 +0545

    Using initial state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './App';
+
 import Props from './Props';
-import SetState from './setState';
 import DefaultProps from './DefaultProps';
+
+import State from './State';
+import SetState from './setState';
+
 import * as serviceWorker from './serviceWorker';
 
 
