c41dbe1fe03bf42b7a373005dd4249ca461b40dc
Sabita neupane <nsabita.sasa@gmail.com>
2019-03-11 21:29:05 +0545

    fetching json data into simple UI component

--- src/reduxExample/Posts.js
+++ src/reduxExample/Posts.js
@@ -4,19 +4,39 @@ import ReactDOM from 'react-dom';
 
 class Posts extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+
     componentWillMount(){
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => this.setState({
+            posts : data
+        }));
     }
 
 
     render() {
         return (
-            <p> Posts </p>
-        );
-      }
+            <div> 
+                {
+                    this.state.posts.map((data) => {
+                        return(
+                            <div key={data.id}>
+                                <h1> {data.title} </h1>
+                                <p> {data.body} </p> <hr/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+      );
     }
+  }
 ReactDOM.render(
   <Posts />, 
   document.getElementById('root')
