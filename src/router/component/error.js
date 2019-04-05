7523e2b1fc89977cf372cdd24aa904d183441ab6
Sabita neupane <nsabita.sasa@gmail.com>
2019-04-05 11:27:11 +0545

    Adding error page and routing unvalid request to error page

---
+++ src/router/component/error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div> Sorry, page not found. </div>
+  );
+}
+
+export default Home;
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from "./component/home";
 import About from "./component/about";
 import Contact from "./component/contact";
+import Error from "./component/error";
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={Home} exact={true} />
-    <Route path="/about" component={About}/>
-    <Route path="/contact" component={Contact}/>
+    <Switch>
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      <Route component={Error}/>
+    </Switch>
   </BrowserRouter>, 
   document.getElementById('root')
 )
